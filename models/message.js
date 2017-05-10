4a90b03a666523b206fdbd5523e19c29f6a6f4c5
Meg Kozak <megkozak@gmail.com>
2017-05-09 20:31:05 -0400

    set up two pages

--- models/message.js
+++ models/message.js
@@ -21,11 +21,11 @@
 
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgresql://localhost/blog_with_sequelize');
+var sequelize = new Sequelize('postgresql://localhost/bulletinboard');
 
 module.exports = sequelize.define('message', {
 	title: Sequelize.STRING,
-	body:  Sequelize.TEXT
+	message:  Sequelize.TEXT
 }, {
 	defaultScope: {
 		order: [['createdAt', 'DESC']]
--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,86 @@
 var express = require('express');
+var Message = require('../models/message')
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index',
-  {
-    title: "message route TITLE",
-    message: "this is meessage"
-  });
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.render('index',
+//   {
+//     title: "message route TITLE",
+//     message: "this is meessage"
+//   });
+// });
+//
+// module.exports = router;
+// var express = require('express');
+//
+// var Post    = require('../models/message');
+//
+// // var models  = require('../models/index');
+// // var Post    = models.post;
+//
+// var router  = express.Router();
+
+// Index.
+router.get('/', function(request, response) {
+	Message.findAll().then(function(messages) {
+		response.render('message/index', {
+			messages: messages
+		});
+	});
+});
+
+// Search.
+router.get('/search', function(request, response) {
+	var query     = request.query.query;
+	var condition = `%${query}%`;
+
+	Message.findAndCountAll({
+		where: {
+			$or: {
+				title: {
+					$iLike: condition
+				},
+				body: {
+					$iLike: condition
+				}
+			}
+		}
+	}).then(function(result) {
+		response.render('message/search', {
+			query: query,
+			count: result.count,
+			posts: result.rows
+		});
+	});
+});
+
+// Create.
+router.post('/', function(request, response) {
+	Message.create({
+		title: request.body.title,
+		message:  request.body.message,
+	}).then(function(post) {
+		response.redirect("/");
+	});
+});
+
+// New.
+router.get('/new', function(request, response) {
+	response.render('message/new');
+});
+
+// Show.
+router.get('/:title', function(request, response) {
+	Message.findOne({
+		where: {
+			title: request.params.title
+		}
+	}).then(function(message) {
+		response.render('message/show', {
+			message: message
+		});
+	});
 });
 
 module.exports = router;
