89705fb43b83fe9d247b63e042af987a584eed9e
Meg Kozak <megkozak@gmail.com>
2017-05-09 23:24:02 -0400

    working bulletin board

--- models/message.js
+++ models/message.js
@@ -1,39 +1,17 @@
-// 'use strict';
-// module.exports = function(sequelize, DataTypes) {
-//   var message = sequelize.define('message', {
-//     title: DataTypes.STRING
-//   }, {
-//     classMethods: {
-//       associate: function(models) {
-//         // associations can be defined here
-//       }
-//     }
-//   });
-//   return message;
-// };
-//
-// module.exports = function(sequelize, Sequelize) {
-// 	return(sequelize.define('message', {
-// 		title: Sequelize.STRING,
-// 		body:  Sequelize.TEXT
-// 	});
-// };
+'use strict';
 
 
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgresql://localhost/bulletinboard');
 
 module.exports = sequelize.define('message', {
-	title: Sequelize.STRING,
-	message:  Sequelize.TEXT
+    title: Sequelize.STRING,
+    message: Sequelize.TEXT
 }, {
-	defaultScope: {
-		order: [['createdAt', 'DESC']]
-		// descending chronological order
-	},
-  getterMethods: {
-		url: function() {
-			return(`/message/${this.title}`);
-		}
-  }
+    defaultScope: {
+        order: [
+            ['createdAt', 'DESC']
+        ]
+
+    }
 });
